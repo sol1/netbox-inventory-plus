d9db410975229ffc15a42f40d4f296d67f7a7be7
Dylan <dylan@sol1.com.au>
2025-05-26 15:02:58 +1000

    bump version

--- netbox_inventory/tests/test_load.py
+++ netbox_inventory/tests/test_load.py
@@ -11,7 +11,7 @@ class NetboxInventoryVersionTestCase(SimpleTestCase):
     """
 
     def test_version(self):
-        assert __version__ == '3.3.4'
+        assert __version__ == '3.4.0'
 
 
 class AppTest(APITestCase):
